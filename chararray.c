8a320c4ce09288a2f15537d59c6ffe9d6d7eca20
Muriel <2029chan.muriel@fcboe.org>
2024-02-07 15:41:35 -0500

    worked on chararray.c

--- chararray.c
+++ chararray.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-	char name[] = "First Last"; // edit this line
-	char user[N];               // create this line and replace N with however many elements you think is necessary
+	char name[] = "Muriel Chan"; // edit this line
+	char user[8];               // create this line and replace N with however many elements you think is necessary
 	
 	// Prompt the user for their name
 	// Use scanf("%s", &user) to store the user's name
+    printf("Enter your name here: ");
 	scanf("%s", &user);
 
-	printf("You name is %s...\n", , );
+	printf("Hello %s. Nice to meet you. My name is %s\n", user, name);
 
 	return 0;
 }
--- intarray.c
+++ intarray.c
@@ -3,13 +3,17 @@
 int main()
 {
 	int arr[5];  // This creates an empy array with 5 elements
-
-    arr[0] = 2;  // Arrays start with element 0
-	arr[1] = 4;
-	arr[2] = 6;  // 3rd element
-	arr[3] = 8;
-	arr[4] = 10; // In a 5 element array, the last element is actually at 4 since the 1st element is at position 0
-
+    printf("Enter the first number: ");
+    scanf("%i", &arr[0]);
+    printf("Enter second number: ");
+    scanf("%i", &arr[1]);
+    printf("Enter the third number: ");
+    scanf("%i", &arr[2]);
+    printf("Enter the fourth number: ");
+    scanf("%i", &arr[3]);
+    printf("Enter the fifth number: ");
+    scanf("%i", &arr[4]);
+    
 	printf("%i %i %i %i %i\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
 	printf("%i %i %i %i %i\n", arr[0] + 1, arr[1] + 1, arr[2] + 1, arr[3] + 1, arr[4] + 1);
 	// write a new printf statement that adds 1 to each array element
